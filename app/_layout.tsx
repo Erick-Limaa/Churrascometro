aeb06a6bd17074007e9a1114462d9d4d3c8c28a0
PedroNSDev <punchabunch3000@gmail.com>
2026-04-06 20:31:06 -0300

    SplashScreen(crua):: Stable

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,8 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useState } from 'react';
+import SplashVideo from '../components/SplashVideo';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,12 +13,18 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [loading, setLoading] = useState(true);
+
+  // SPLASH PRIMEIRO >>
+  if (loading) {
+    return <SplashVideo onFinish={() => setLoading(false)} />;
+  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
